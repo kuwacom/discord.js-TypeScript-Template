19a1d93da4eb318037b2a4f8035501cf481d1145
kuwacom <kuwa.com3@gmail.com>
2025-03-16 19:41:06 +0900

    update: 余計な env を削除

--- src/configs/env.ts
+++ src/configs/env.ts
@@ -3,7 +3,6 @@ const env = {
     bot: {
         prefix: process.env.BOT_PREFIX as string,
         token: process.env.BOT_TOKEN as string,
-        noticeChannelId: process.env.BOT_NOTICE_CHANNEL_ID as string
     }
 }
 
